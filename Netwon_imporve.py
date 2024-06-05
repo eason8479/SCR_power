9536d2a187bec44b58bbfe1701e58254f284f410
Eason <lineason321123@gmail.com>
2024-06-05 13:27:40 +0800

    merge_06_05_1

--- Netwon_imporve.py
+++ Netwon_imporve.py
@@ -5,20 +5,20 @@ import math
 p = 250
 
 def linear(x):
-    y = 1 - (x / np.pi)
+    y = np.pi - x
     return y
 
 def Diff_func(x):
-    y = (np.cos(2*x) - 1) / np.pi
+    y = np.cos(2*x) - 1
     return y
 
 def SCR_function(x, a):
-    y = 1 + np.sin(2*x)/(2*np.pi) - x/np.pi
+    y = np.pi + np.sin(2*x)/2 - x
     y = y-a
     return y
 
 def Netwon(x_guess, a):
-    for i in range (8):
+    for i in range (10):
         x_guess = x_guess - (SCR_function(x_guess, a)/(Diff_func(x_guess)))
         x_guess = x_guess % np.pi
     x_ans = x_guess
@@ -28,22 +28,17 @@ x = np.linspace(0, np.pi, 100)
 x = x[1:-1]
 y_target = SCR_function(x, 0)
 
+plt.plot(x, y_target)
+plt.show()
+
 x_guess = linear(x)
-x_ans1 = Netwon(x_guess, y_target)
-y_ans1 = SCR_function(x_ans1, 0)
+x_ans = Netwon(x_guess, y_target)
+y_ans = SCR_function(x_ans, 0)
 
-x_ans2 = Netwon(1e-6, y_target)
-y_ans2 = SCR_function(x_ans2, 0)
-
-'''
 plt.plot(x_ans, y_ans)
 plt.plot(x, y_target)
-'''
-y_diff1 = y_ans1 - y_target
-y_diff1 = 250*y_diff1
-y_diff2 = y_ans2 - y_target
-y_diff2 = 250*y_diff2
+plt.show()
 
-plt.plot(x, y_diff1)
-plt.plot(x, y_diff2)
+y_diff = y_ans - y_target
+plt.plot(250*y_target, 250*y_diff)
 plt.show()
